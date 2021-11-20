2010d01fe126f3c50993a186ed6e9bc57b71424f
BrookeDougherty <BrookeDougherty@users.noreply.github.com>
2021-11-20 21:22:10 +0000

    deploy: 10a6bcf8938fca181954186b6f4e235cf53389af

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
+// BACK TO TOP BUTTON
+//Get the button
+var mybutton = document.getElementById("myBtn");
 
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+// ROTATION OF IMAGES
+
+const maxRotation = 365;
+function handleScroll() {
+  console.log(window.scrollY);
+  // Get list of albums
+  const albums = document.querySelectorAll(".album");
+  console.log(albums);
+
+  // Loop through all albums and change rotation
+  for (let i = 0; i < albums.length; i++) {
+    const album = albums[i];
+    album.style = `transform: rotate(${window.scrollY}deg)`;
+  }
+  // album.style = `transform: rotate(${window.scrollY}deg)`;
+}
+window.onscroll = handleScroll;
