eb16689198229a619022a967dc01373e1bea38ad
BrookeDougherty <BrookeDougherty@users.noreply.github.com>
2021-11-23 23:06:17 +0000

    deploy: 3f2baf724fc38dcb508dfad1aae522ea6ff6c75e

--- js/main.js
+++ js/main.js
@@ -45,27 +45,48 @@ window.onscroll = handleScroll;
 //get
 const recordButtons = document.querySelectorAll(".album");
 // for loop
-for (let index = 0; index < recordButtons.length; index++) {
+for (let index = 0; index < recordButtons.length; index = index + 1) {
   const recordButton = recordButtons[index];
   // Defining function for button click
   function buttonClick() {
     console.log(index);
+
+    hideRecords(index);
+
     const recordRow = document.querySelector(".record-row-" + (index + 1)); //record-row-1
-    recordRow.classList.toggle("visible");
+    recordRow.classList.add("visible");
     recordRow.scrollIntoView();
   }
   recordButton.addEventListener("click", buttonClick);
 }
 
 // ACCORDIAN SECTION
-// get
-const accordian = document.querySelector(".album-accordian");
-console.log(accordian);
-//define
-function accordianClick() {
-  console.log("album info");
-  const songlistPanel = document.querySelector(".songlist-panel");
-  songlistPanel.classList.toggle("visible");
+// Get
+const accordians = document.querySelectorAll(".album-accordian");
+for (let index = 0; index < accordians.length; index = index + 1) {
+  const accordian = accordians[index];
+  console.log(accordian);
+  // Defining function for accordian button click
+  function accordianClick() {
+    const songlistPanel = document.querySelector(
+      ".songlist-panel-" + (index + 1)
+    );
+    songlistPanel.classList.toggle("visible");
+    songlistPanel.scrollIntoView();
+  }
+  // Wire up = reveal accordian information when I click on accordian button
+  accordian.addEventListener("click", accordianClick);
 }
-// wire up = reveal accordian information when I click on accordian button
-accordian.addEventListener("click", accordianClick);
+
+// Hide Un-needed Records
+function hideRecords(indexToShow) {
+  console.log(indexToShow);
+  // Get Record Rows
+  const recordRows = document.querySelectorAll(".record-row");
+  for (let index = 0; index < recordRows.length; index = index + 1) {
+    if (index != indexToShow) {
+      const recordRow = recordRows[index];
+      recordRow.classList.remove("visible");
+    }
+  }
+}
